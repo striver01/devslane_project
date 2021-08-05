6e3de92f3fed23c95ca64a1cc542b561ec92779d
shubh-coder22 <shubh.purwar_cs19@gla.ac.in>
2021-08-06 00:44:09 +0530

    modification to the AvtarStack

--- src/Components/AvtarStack/AvtarStack.tsx
+++ src/Components/AvtarStack/AvtarStack.tsx
@@ -8,30 +8,33 @@ interface Props {
 const AvtarStack: FC<Props> = ({ stacksize }) => {
   let size = 0;
   let stack: string[] = [pic, pic, pic, pic];
-  let val = [pic],
-    show = true;
+  let val = [pic];
   if (stacksize > 4) {
     size = stacksize - 4;
-    show = true;
     stacksize = 4;
   }
-  if (stacksize <= 4) {
-    val = stack.slice(0, stacksize);
-    show = false;
-  }
+  val = stack.slice(0, stacksize);
 
   return (
-    <div className="relative flex">
+    <div className="flex">
       {val.map((res: string, key: number) => {
         return (
-          <img
-            className={`w-20 h-20 rounded-full border-2 border-white transform hover:-translate-y-2 duration-500`}
-            src={res}
-            alt="none"
-          />
+          <div className="-space-x-2">
+            <img
+              className={`inline-block w-20 h-20 rounded-full border-2  border-white transform hover:-translate-y-2 duration-500 relative right-${
+                key * 4
+              }`}
+              src={res}
+              alt="none"
+            />
+          </div>
         );
       })}
-      <p className="rounded-full border border-black text-center px-2 left-96 my-auto bg-white transform hover:-translate-y-2 duration-500">
+      <p
+        className={`rounded-full border border-black text-center px-2 my-auto bg-white transform hover:-translate-y-2 duration-500 relative right-${
+          stacksize * 4
+        }`}
+      >
         {size}+more
       </p>
     </div>
